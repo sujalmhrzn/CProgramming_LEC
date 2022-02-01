168691ece79942590d72f279362214577f801468
Sujal Maharjan <sujal.mhrzn7@outlook.com>
2022-02-01 12:28:52 +0545

    first switch

---
+++ chapter3_CBasics/switch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main()
+{
+  char grade;
+  printf("Enter your marks obtained : \n");
+  scanf("%c", &grade);
+
+  switch (grade)
+  {
+  case 'A':
+    printf("Excellent\n");
+    break;
+  case 'B':
+  case 'C':
+    printf("Well done\n");
+    break;
+  case 'D':
+    printf("You passed\n");
+    break;
+  case 'F':
+    printf("Better try again\n");
+    break;
+  default:
+    printf("Invalid Grade\n");
+    break;
+  }
+  printf("Your grade is %c", grade);
+}
