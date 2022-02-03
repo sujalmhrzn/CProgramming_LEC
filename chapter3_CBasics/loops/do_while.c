3430d7b974b675b45ffc968e3de1ccbb434f621c
Sujal Maharjan <sujal.mhrzn7@outlook.com>
2022-02-03 11:37:20 +0545

    first whileloop

---
+++ chapter3_CBasics/loops/do_while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+    int a=10;
+    do 
+    {
+        printf("value of A %d\n",a);
+        a++;
+
+    }while (a<20);
+    
+
+}
---
+++ chapter3_CBasics/loops/while.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    while(a<20)
+    {
+        printf("value of A %d\n",a);
+        a++;
+    }
+}
