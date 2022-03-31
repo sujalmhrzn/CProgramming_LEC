4f4772ffc14f6212b30db6eb44dc8817dc45b10a
Sujal Maharjan <sujal.mhrzn7@outlook.com>
2022-03-31 10:09:15 +0545

    dianamic memory

---
+++ Chapter6_pointer/dianamic_memory.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+ void main(){
+     int *ptr,number,sum=0,i;
+     printf("enter the size of numbers:");
+     scanf("%d",&number);
+     ptr=(int*)malloc(number*sizeof(int));
+
+     for(i=0;i<number;i++)
+     {
+         printf("\nenter the numbers for  addition");
+         scanf("%d", ptr + i );
+         sum=sum+*(ptr+i);
+     }
+     printf("the sum of number is: %d",sum);
+     free(ptr);
+     
+
+ }
